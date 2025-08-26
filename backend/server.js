8ec76a0b0e4e5f9d07e8dc808c83537368ae854a
createdbyharsh <createdbyharsh@gmail.com>
2025-08-26 17:00:38 +0530

    authRouter and auth middleware

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import authRouter from "./routes/authRouter.js";
 
 dotenv.config({ path: "../.env" });
 
 const PORT = process.env.PORT;
 const app = express();
+
+app.use(express.json());
+app.use("/api/users", authRouter);
 
 app.get("/", (req, res) => {
   res.send("working");
